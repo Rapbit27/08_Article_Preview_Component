0bc99016e0af2b68ba292299ebe92d2dcf1d5475
Robel T. Hawelti <96294753+Rapbit27@users.noreply.github.com>
2024-08-25 18:41:24 +0300

    script for toggle
    added the script that would change the user name with the share links in mobile mode.

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
-const bottomLane = document.getElementsByClassName("bottom-lane");
-const shareBtn = document.getElementsByClassName("share-btn");
-
+document.addEventListener('DOMContentLoaded', () => {
+    const shareBtn = document.querySelector('.share-btn');
+    const author = document.querySelector('.author');
+    const shareLinks = document.querySelector('.share-links');
+    const bottomLane = document.querySelector('.bottom-lane'); // Line added to select the .card element
+  
+    if (author.style.display === 'none') {
+      bottomLane.style.backgroundColor = 'hsl(214, 17%, 51%)'; // Line added to set initial background color
+    }
+  
+    if (shareBtn && author && shareLinks) {
+      shareBtn.addEventListener('click', () => {
+        if (author.style.display === 'none' || !author.style.display) {
+          author.style.display = 'flex';
+          shareLinks.style.display = 'none';
+          bottomLane.style.backgroundColor = 'hsl(0, 0%, 100%)'; // Change background color to default
+        } else {
+          author.style.display = 'none';
+          shareLinks.style.display = 'flex';
+          bottomLane.style.backgroundColor = 'hsl(214, 17%, 51%)'; // Change background color when share-links is visible
+        }
+      });
+    }
+  });
